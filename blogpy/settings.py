9cef1ee5a378faedf5f6fd7169db2941c85a5af3
AfshinAt <cjabram@protonmail.ch>
2022-10-06 16:14:22 -0700

    persian

--- blogpy/settings.py
+++ blogpy/settings.py
@@ -104,9 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'fa'
 
-TIME_ZONE = 'EST'
+TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
